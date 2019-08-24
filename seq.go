ab646dbbdf2b5d227206bda054612be496fcc6e7
Michael Bruce <me@michaelbruce.co>
2019-08-24 23:24:57 +0100

    simple map created

--- seq.go
+++ seq.go
@@ -3,9 +3,16 @@ package seq
 type Seq struct{ xs []string }
 
 func New(e string) Seq {
-	return Seq{xs: []string{}}
+	return Seq{xs: []string{e}}
 }
 
-func (s *Seq) ToSlice() []string {
+func (s Seq) Map(f func(string) string) Seq {
+	for i, x := range s.xs {
+		s.xs[i] = f(x)
+	}
+	return s
+}
+
+func (s Seq) ToSlice() []string {
 	return s.xs
 }
--- seq_test.go
+++ seq_test.go
@@ -2,13 +2,19 @@ package seq_test
 
 import (
 	"seq"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSeqCreated(t *testing.T) {
-	s := seq.New("e")
+	assert.Equal(t, seq.New("e").ToSlice(), []string{"e"})
+}
 
-	assert.Equal(t, s.ToSlice(), []string{})
+func TestMap(t *testing.T) {
+	s := seq.New("E")
+
+	s.Map(strings.ToLower)
+	assert.Equal(t, s.Map(strings.ToLower).ToSlice(), []string{"e"})
 }
